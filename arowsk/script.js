a4d9a294a8109f79e694087d71b792b7bacf4ddb
Carlo Colombo <carlo.colombo@gmx.com>
2012-06-06 21:38:41 +0200

    working curry on firefox mobile

--- arowsk/script.js
+++ arowsk/script.js
@@ -1,8 +1,9 @@
 (function($) {
 	$.domReady(function() {
-		var count = localStorage.getItem("count") || parseInt(prompt("Arrows?")) || 50,
-			button = $('#button'),
+		var count 	= localStorage.getItem("count") || parseInt(prompt("Arrows?")) || 50,
+			button 	= $('#button'),
 			counter = $('#counter'),
+			curry	= $('#curry')[0],
 			setCount = function(c){
 				localStorage.setItem("count", c)
 				counter.text(c)	
@@ -15,8 +16,9 @@
 				setCount(--count)
 				button.addClass('opacity')
 				setTimeout(function(){
-					button.removeClass('opacity')
+					//button.removeClass('opacity')
 				},250)
+				curry.play()
 			}
 		})
 
